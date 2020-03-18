f8e26a4931837863d7c8f1d697b2f046f5582f43
RichardEsquivel <rlesquivel@gmail.com>
2020-03-18 14:47:15 -0500

    fixed issue where code was accidentally deleted during commenting, passes all tests

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -15,6 +15,12 @@ def merge(arrA, arrB):
         if arrA[left] < arrB[right]:
             merged_arr.append(arrA[left])
             left += 1
+        else:
+            merged_arr.append(arrB[right])
+            right += 1
+    while left < len(arrA):
+        merged_arr.append(arrA[left])
+        left += 1
     while right < len(arrB):
         merged_arr.append(arrB[right])
         right += 1
@@ -25,23 +31,26 @@ def merge(arrA, arrB):
 def merge_sort(arr):
     # TO-DO
     # Establish the base case of array length smaller than 1 where merge_sort will end
-    if len(arr) <= 1
-    return arr
+    if len(arr) <= 1:
+        return arr
 
    # Create a middle point to split the array up and use // to floor divide down to an appropriate
    # index value
-   split_array= len(arr) //2
+    split_array = len(arr) // 2
    # establish reference for left and right side of array and recursively call merge_sort function
    # pass in values from beginning of array to the index split point and from the index split point to the end of the array on the right side
-    left_side= merge_sort(arr[:split_array])
+    left_side = merge_sort(arr[:split_array])
     right_side = merge_sort(arr[split_array:])
     # return by calling merge function within merge_sort and join left and right side
     return(merge(left_side, right_side))
 
+
 # test with print by passing in a list of values
-print(merge_sort([99, 32, 86, 54, 33, 11111, 2, 8, 99, 103]))
+print(merge_sort([99, 32, 86, 54, 33, 11111, 2, 8, 103, 100]))
 
 # STRETCH: implement an in-place merge sort algorithm
+
+
 def merge_in_place(arr, start, mid, end):
     # TO-DO
 
